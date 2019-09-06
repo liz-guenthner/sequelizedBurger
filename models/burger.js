bffac32dce5d0c2c321b53e9074306a1870bea96
Liz Guenthner <liz.guenthner@t-3.com>
2019-09-05 21:43:17 -0500

    reloaded node modules and added routes files

--- models/burger.js
+++ models/burger.js
@@ -1,42 +1,16 @@
-// // Sequelize (capital) references the standard library
-var Sequelize = require("sequelize");
-// // sequelize (lowercase) references our connection to the DB.
-var sequelize = require("../config/connection.js");
-
 module.exports = function(sequelize, DataTypes) {
   var Burger = sequelize.define("Burger", {
     burger_name: {
-      type: DataTypes.STRING
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
     },
     devoured: {
-      type: DataTypes.INTEGER,
+      type: DataTypes.TINYINT,
       defaultValue: false
     }
   });
   return Burger;
-}
-
-
-// Creates a "Character" model that matches up with DB
-// var Burger = sequelize.define("burger", {
-//   // the routeName gets saved as a string
-//   burger_name: Sequelize.STRING,
-//   // the name of the character (a string)
-//   name: Sequelize.STRING,
-//   // the character's role (a string)
-//   role: Sequelize.STRING,
-//   // the character's age (a string)
-//   age: Sequelize.INTEGER,
-  
-//   // and the character's force points (an int)
-//   forcePoints: Sequelize.INTEGER
-// }, {
-//   // disable the modification of tablenames; By default, sequelize will automatically
-//   // transform all passed model names (first parameter of define) into plural.
-//   // if you don't want that, set the following
-//   freezeTableName: true
-// });
-
-
-// Burger.sync();
-// module.exports = Burger;
+};
--- server.js
+++ server.js
@@ -1,19 +1,34 @@
+// *****************************************************************************
+// Server.js - This file is the initial starting point for the Node/Express server.
+//
+// ******************************************************************************
+// *** Dependencies
+// =============================================================
 var express = require("express");
 
+// Sets up the Express App
+// =============================================================
 var app = express();
+var PORT = process.env.PORT || 8080;
 
-// Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static("public"));
+// Requiring our models for syncing
+var db = require("./models");
 
 // Parse request body as JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-require("./controllers/burgers_controller");
+// Static directory
+app.use(express.static("public"));
 
-// listen on port 3000
-var PORT = process.env.PORT || 3000;
+// Routes
+// =============================================================
+require("./routes/api-routes.js")(app);
 
-app.listen(PORT, function() {
-  console.log("App listening on PORT " + PORT);
+// Syncing our sequelize models and then starting our Express app
+// =============================================================
+db.sequelize.sync({ force: true }).then(function() {
+  app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
 });
